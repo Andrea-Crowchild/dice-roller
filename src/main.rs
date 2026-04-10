e5f288bcacdf9056ad9ae7f9ba2535450e706ee3
Andrea-Crowchild <noisysunset@gmail.com>
2026-04-10 18:11:02 -0400

    Initial commit - working dice roller

---
+++ src/main.rs
@@ -0,0 +1,41 @@
+use std::env;
+use rand::Rng;
+
+
+
+fn main() {
+    // initialize sum variable to 0 
+    let mut sum: i32 = 0;
+    //take command line arguments
+    let args: Vec<String> = env::args().collect();
+    if args[1] == "-h" {
+        println!("Syntax is dice <no> <sides>");
+        return;
+    }
+    if args.len() < 2 {
+        println!("Invalid input. Use -h for help.");
+        return;
+    }
+    let no_of_dice: u32 = match args[1].parse(){
+        Ok(n)  => n,
+        Err(_) => {
+            println!("Invalid input. Use -h for help.");
+            return;
+        }
+    };
+    let sides: u32 = match args[2].parse(){
+        Ok(n) => n,
+        Err(_) => {
+            println!("Invalid input. Use -h for help.");
+            return;
+        }
+    };
+    // rolling logic
+    for _i in 0..no_of_dice{
+        let result = rand::thread_rng().gen_range(1..=sides);
+        sum += result as i32;
+        println!("{}", result);
+    }
+    println!("Total: {}", sum);
+
+}
